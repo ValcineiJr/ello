fa3705ad7633cfade7b982f2f527721093bee5c7
Valcinei Jr <hakjog@gmail.com>
2020-11-09 05:46:15 -0300

    Adicionando o Login

--- src/pages/Painel/index.js
+++ src/pages/Painel/index.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import { Container } from "./styles";
+import api from "../../services/api";
 
 function Painel() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(localStorage.getItem("@login/token"));
+
+  const send = async (e) => {
+    e.preventDefault();
+    const response = await api.post("login", { username, password });
+
+    if (response.data.msg === "Sucesso") {
+      localStorage.setItem("@login/token", true);
+      setLogin(true);
+    } else {
+      alert(response.data.msg);
+    }
+  };
+
+  useEffect(() => {
+    // localStorage.clear();
+  }, []);
+
   return (
     <Container>
-      <Header />
-      <div className="welcome">
-        <p></p>
-      </div>
+      {login && <Header />}
+
+      {!login && (
+        <div className="welcome">
+          <form method="post" onSubmit={send}>
+            <h1>Login</h1>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Usuário"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Senha"
+            />
+            <input type="submit" value="Entrar" />
+          </form>
+        </div>
+      )}
     </Container>
   );
 }
--- src/pages/Painel/styles.js
+++ src/pages/Painel/styles.js
@@ -5,155 +5,31 @@ export const Container = styled.div`
   min-height: 100vh;
   flex-direction: column;
   background: #03172f;
-`;
-export const Header = styled.div`
-  height: 50px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 30px;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  background: #03172f;
-  color: #fff;
 
-  ul {
-    display: flex;
-  }
-  li {
-    padding: 10px;
-    color: #fff;
-  }
-
-  span {
-    font-size: 30px;
-  }
-`;
-export const Content = styled.div`
-  form {
-    width: 100%;
-    max-width: 1000px;
-    margin: 0 auto;
+  .welcome {
+    height: 80vh;
     display: flex;
     flex-direction: column;
-  }
-
-  .images {
-    /* background:red; */
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-
-    img {
-      width: calc(25% - 40px);
-      margin: 5px;
-    }
-  }
-
-  p {
-    text-align: center;
-    padding: 20px 0;
-    color: #fff;
-    text-transform: uppercase;
-    font-size: 20px;
-  }
-
-  input,
-  textarea,
-  select {
-    width: 80%;
-    height: 40px;
-    border: 0;
-    margin: 10px auto;
-    padding-left: 8px;
-    border-radius: 5px;
-  }
-
-  .header {
-    display: flex;
-    margin: 0 auto;
-    width: 80%;
     justify-content: center;
     align-items: center;
-
-    a {
-      padding: 0 16px;
-      color: #fff;
-      height: 40px;
-      cursor: pointer;
-      margin-left: 8px;
-      background: #dc3545;
-      border-radius: 5px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
   }
 
-  select {
-    font-weight: bold;
-    font-size: 18px;
+  form {
+    display: flex;
+    flex-direction: column;
     width: 100%;
+    max-width: 500px;
+  }
+
+  h1 {
+    color: #fff;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  input {
+    height: 40px;
+    padding-left: 14px;
     margin: 10px 0;
   }
-
-  textarea {
-    padding-top: 8px;
-  }
-
-  input[type="file"] {
-    display: none;
-  }
-  label {
-    padding: 0 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 50px;
-    border: 0;
-    margin: 10px auto;
-    border-radius: 5px;
-    color: #fff;
-    background: #4bbdd8;
-  }
-
-  img.logo {
-    width: 150px;
-    margin: 10px auto;
-  }
-
-  button {
-    width: 120px;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 50px;
-    border: 0;
-    margin: 10px auto;
-    border-radius: 5px;
-    background: #4bbdd8;
-    text-transform: uppercase;
-  }
-`;
-export const SessionSelector = styled.div`
-  height: 50px;
-  width: 100%;
-  max-width: 1000px;
-  justify-content: space-between;
-  display: flex;
-  margin: 40px auto 20px;
-`;
-
-export const Tab = styled.a`
-  width: 49%;
-  display: flex;
-  background: ${(props) => props.backgroundColor};
-  justify-content: center;
-  border-radius: 5px;
-  align-items: center;
-  cursor: pointer;
-  text-transform: uppercase;
-  color: #fff;
-  transition: all 0.6s;
 `;
